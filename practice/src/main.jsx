52789cb4777d0c2f4b1173c5b9345e5a55c2a341
Rafsan-Swadhin <rafsanjaniahmed84@gmail.com>
2025-03-18 21:36:22 +0600

    Initial commit

---
+++ practice/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+   <div>
+   <h1>heljfgo</h1>
+   <h3>family</h3>
+   </div>
+  </StrictMode>,
+)
