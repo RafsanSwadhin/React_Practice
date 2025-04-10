4b61ca2c5d1e89f21bdb1c48a3ed8ff63a74939a
Rafsan-Swadhin <rafsanjaniahmed84@gmail.com>
2025-04-10 22:42:20 +0600

    json data update

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react"
 import Card from "./components/Card"
+import Data from "./data.json"
 function App(){
 
   return <div>
      <h1 className='headingStyle largeText'>Todo App</h1>
-    <Card titleText = "call mother" descText = "jfhs hfd f hkfsff hkfksdf "/>
-    <Card titleText = "call father " descText = "the desc number two "/>
+    <Card titleText = {Data[0].title} descText ={Data[0].desc}/>
+    <Card titleText = {Data[1].title} descText ={Data[1].desc}/>
+    <Card titleText = {Data[2].title} descText ={Data[2].desc}/>
+    <Card titleText = {Data[3].title} descText ={Data[3].desc}/>
 
   </div>
 
