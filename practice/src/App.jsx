49d6e0419b9d78b2756981ac85d1041bead11ca9
Rafsan-Swadhin <rafsanjaniahmed84@gmail.com>
2025-04-11 12:36:48 +0600

    push data using for loop

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -2,13 +2,18 @@ import React from "react"
 import Card from "./components/Card"
 import Data from "./data.json"
 function App(){
+  let items = []
+  for ( let x = 0 ; x<Data.length; x++){
+    items.push(<Card titleText={Data[x].title} descText={Data[x].desc}/>)
+  }
 
   return <div>
      <h1 className='headingStyle largeText'>Todo App</h1>
-    <Card titleText = {Data[0].title} descText ={Data[0].desc}/>
+     {items}
+    {/* <Card titleText = {Data[0].title} descText ={Data[0].desc}/>
     <Card titleText = {Data[1].title} descText ={Data[1].desc}/>
     <Card titleText = {Data[2].title} descText ={Data[2].desc}/>
-    <Card titleText = {Data[3].title} descText ={Data[3].desc}/>
+    <Card titleText = {Data[3].title} descText ={Data[3].desc}/> */}
 
   </div>
 
