ebd4ba26cb2965118b719c208d0b746e64513567
Rafsan-Swadhin <rafsanjaniahmed84@gmail.com>
2025-04-11 17:39:35 +0600

    Map

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -2,19 +2,11 @@ import React from "react"
 import Card from "./components/Card"
 import Data from "./data.json"
 function App(){
-  let items = []
-  for ( let x = 0 ; x<Data.length; x++){
-    items.push(<Card titleText={Data[x].title} descText={Data[x].desc}/>)
-  }
+
 
   return <div>
      <h1 className='headingStyle largeText'>Todo App</h1>
-     {items}
-    {/* <Card titleText = {Data[0].title} descText ={Data[0].desc}/>
-    <Card titleText = {Data[1].title} descText ={Data[1].desc}/>
-    <Card titleText = {Data[2].title} descText ={Data[2].desc}/>
-    <Card titleText = {Data[3].title} descText ={Data[3].desc}/> */}
-
+     {Data.map((item,index)=> <Card key={index} titleText={item.title} descText ={item.desc}/>)}
   </div>
 
 }
