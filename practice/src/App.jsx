c5855154a14173ff5e80b5d1feb3a393fd55bcdc
Rafsan-Swadhin <rafsanjaniahmed84@gmail.com>
2025-04-15 15:43:58 +0600

    event handler

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Condition from "./condition";
+import Event_Handler_Class from "./Event_Handler_Class";
 function App(){
   return (
     <div>
     <Condition/>
+    <Event_Handler_Class/>
     </div>
   )
 }
---
+++ practice/src/Event_Handler_Class/index.jsx
@@ -0,0 +1,28 @@
+import React,{Component} from "react";
+
+class Event_Handler_Class extends Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+            changedValue:''
+        }
+    }
+
+    handleOnChange = (e) =>{
+        this.setState({
+            changedValue : e.target.value          
+        })
+    }
+    render(){
+        return(
+            <div>
+                <h1>hjkhgjj</h1>
+                <input type="text" onChange={this.handleOnChange} />
+                <p>{this.state.changedValue}</p>
+            </div>
+        )
+    }
+}
+
+export default Event_Handler_Class
