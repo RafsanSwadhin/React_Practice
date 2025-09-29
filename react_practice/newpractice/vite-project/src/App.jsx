530527eb038a363d98c16d9f8165291844ac9cc1
Rafsan-Swadhin <rafsanjaniahmed84@gmail.com>
2025-09-29 22:52:56 +0600

    react useState revision

--- react_practice/newpractice/vite-project/src/App.jsx
+++ react_practice/newpractice/vite-project/src/App.jsx
@@ -1,9 +1,14 @@
-import { Navbar } from "./components/Navbar"
+import { useState } from "react"
 
 const App = () => {
+  const [x, setx] = useState(0)
+  const btnClick = () =>{
+    setx(x+1)
+  }
   return (
     <div>
-      <Navbar/>
+      {x}
+      <button onClick={() => {btnClick}}>Click Here</button>
     </div>
   )
 }
