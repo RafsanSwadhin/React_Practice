6326153360faba00aa11eac81877cc161e5377be
Rafsan-Swadhin <rafsanjaniahmed84@gmail.com>
2025-09-30 22:08:23 +0600

    outputting list

--- react_practice/newpractice/vite-project/src/App.jsx
+++ react_practice/newpractice/vite-project/src/App.jsx
@@ -1,14 +1,27 @@
 import { useState } from "react"
 
 const App = () => {
-  const [x, setx] = useState(0)
-  const btnClick = () =>{
-    setx(x+1)
-  }
+  const [blogs, setBlogs] = useState([
+    {title:"My new website", body: 'lorem epsum,,,,,,,', author: 'Rafsan',id :1},
+    {title:"Welcome party vhia", body: 'lorem epsum,,,,,,,', author: 'Jani',id :2},
+    {title: "web dev needed", body: 'lorem epsum,,,,,,,', author: 'Skib',id :3},
+  ])
+
+
+
   return (
-    <div>
-      {x}
-      <button onClick={() => {btnClick}}>Click Here</button>
+    <div className="home">
+      {blogs.map((blog) => (
+          <div className="blog-preview" key={blog.id}>
+            <h2>{blog.title}</h2>
+            <p>written by {blog.author}</p>
+            
+
+
+          </div>
+      ))
+
+      }
     </div>
   )
 }
